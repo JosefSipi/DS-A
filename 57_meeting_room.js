e50c94898329ce60ee5590319a1b22d0a5a36894
Josef <josephsipiorski@gmail.com>
2024-04-09 23:28:13 -0500

    update

--- 57_meeting_room.js
+++ 57_meeting_room.js
@@ -1,31 +1,40 @@
 // Function to test your solution
 function minMeetingRooms(intervals) {
+
+    if(intervals.length === 0) return 0
     // Your solution goes here
-    let numMeetingRooms = 0;
+    let numMeetingRooms = 1;
     let startTimeSmallest = undefined;
     let endTimeLargest = undefined;
 
     intervals.forEach((meeting) => {
+        // console.log(meeting)
         let startTime = meeting[0];
         let endTime = meeting[1];
 
-        if(startTimeSmallest === undefined){
+        console.log(startTime)
+        // console.log(endTime)
+
+
+        if(startTimeSmallest != undefined){
+            if(startTime < endTimeLargest){
+                numMeetingRooms = numMeetingRooms + 1
+                endTimeLargest = endTime
+                startTimeSmallest = startTime
+            } else {
+                startTime = startTime
+                endTime = endTime
+            }
+        } else {
             startTimeSmallest = startTime
             endTimeLargest = endTime
-            return
         }
 
-        if(startTime < startTimeSmallest){
-            numMeetingRooms += 1
-        } else {
-            startTime = startTime
-            endTime = endTime
-        }
 
-        // I need to find total number of meetings that overlap at one time
-        // my solution assumes all meetings are listed in order of earliest to latest start times
-
+   
     });
+
+    console.log(numMeetingRooms + "min num meetings required")
 
     return numMeetingRooms;
 }
@@ -54,8 +63,12 @@ const testCases = [
     },
     {
         input: [[0, 30], [20, 40]],
-        expected: 1
-    }
+        expected: 2
+    },
+    {
+        input: [[0, 30], [15, 20], [25,50], [50, 100]],
+        expected: 2
+    },
 ];
 
 // Function to run tests
